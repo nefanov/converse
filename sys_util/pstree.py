3e3cbd343c77240ec7caeb4b6b3340bb9b4c6fe0
nefanov <efanov@phystech.edu>
2018-02-19 17:40:05 +0300

    Update pstree.py

--- sys_util/pstree.py
+++ sys_util/pstree.py
@@ -1,9 +1,11 @@
 import psutil
 
-for proc in psutil.process_iter():
-     try:
-         pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-     except psutil.NoSuchProcess:
-         pass
-     else:
-         print(pinfo)
+# process_list
+def process_list():
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            print(pinfo)
