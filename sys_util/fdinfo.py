27f55b175f21f90e8daf477357ca1a5d0134d898
nefanov <efanov@phystech.edu>
2018-05-18 17:59:03 +0300

    sockets added

--- sys_util/fdinfo.py
+++ sys_util/fdinfo.py
@@ -1,8 +1,12 @@
-import sys, os, psutil, fnctl
-import statvfs
+import sys, os, psutil
+import fnctl, statvfs, socket
 
 # returns max filename length according to the fs type
 def fn_len_loc(path)
     return os.statvfs(path)[statvfs.F_NAMEMAX]
+
+def get_sock_info(fd):
+    host, port = socket.getsockname()
+    return (host, port)
 # res = fnctl(...)
 # fd, F_GETPATH, filePath
