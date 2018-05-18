0ff3712f03f29f76503ac19fe79871c4ecf6d811
nefanov <efanov@phystech.edu>
2018-05-18 17:45:35 +0300

    Update fdinfo.py

--- sys_util/fdinfo.py
+++ sys_util/fdinfo.py
@@ -1,4 +1,8 @@
 import sys, os, psutil, fnctl
+import statvfs
 
+# returns max filename length according to the fs type
+def fn_len_loc(path)
+    return os.statvfs(path)[statvfs.F_NAMEMAX]
 # res = fnctl(...)
 # fd, F_GETPATH, filePath
