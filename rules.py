43c72288855711a1575f7646f30ddda89c9c6333
nefanov <efanov@phystech.edu>
2018-06-08 14:33:15 +0300

    Update rules.py
    ref

--- rules.py
+++ rules.py
@@ -1,6 +1,6 @@
 
 def fork(proc_entry):
-        proc_entrt.call = 'fork'
+        proc_entry.call = 'fork'
         return proc_entry
 
 def setsid()
@@ -15,25 +15,30 @@ apply_rule = {
         'setpgid':setpgid
 }
 
+
+
 def check_cf(child, parent):
+        def compose_rules_cf():
+                setsid_cont = []
+                setpgid_cont = []
+                fork_cont = []
+                setsid_cont.append({'eq':['child.g','child.s', 'child.g']})
+                setpgid_cont.append({'eq':['child.g','child.p']})
+                setpgid_cont.append({'neq':['child.s','child.p']})
+
+                fork_cont.append({'eq':['child.g','parent.g']})
+                fork_cont.append({'eq':['child.s','parent.s']})
+                headers=['setsid','setpgid','fork']
+                hierarch_list = [setsid_cont, setpgid_cont, fork_cont]
+                return headers, hierarch_list
+
         def select_from(allias, child, parent):
                 if allias[0] == 'child':
                         return child
                 else:
                         return parent
-# rules list composing
-        setsid_cont = []
-        setpgid_cont = []
-        fork_cont = []
-        setsid_cont.append({'eq':['child.g','child.s', 'child.g']})
-        setpgid_cont.append({'eq':['child.g','child.p']})
-        setpgid_cont.append({'neq':['child.s','child.p']})
 
-        fork_cont.append({'eq':['child.g','parent.g']})
-        fork_cont.append({'eq':['child.s','parent.s']})
-        headers=['setsid','setpgid','fork']
-        hierarch_list = [setsid_cont, setpgid_cont, fork_cont]
-
+        headers, hierarch_list = compose_rules_cf()
         for idx, rule in enumerate[hierarch_list]:
                 corr = True
                 for c in rule:
